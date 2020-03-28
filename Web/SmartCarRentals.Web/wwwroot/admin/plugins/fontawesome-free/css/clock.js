8eee49dc5e6f882a41f25855ef139d2223f68f4f
ulivegenov <uli_vegenov@abv.bg>
2020-03-28 15:54:36 +0200

    Refactoring in wwwroot folder.

--- Web/SmartCarRentals.Web/wwwroot/admin/plugins/fontawesome-free/css/clock.js
+++
@@ -1,7 +0,0 @@
-﻿var s, t; s = document.createElement("script"); s.type = "text/javascript";
-s.src = "//cdn.dayspedia.com/js/dwidget.min.vef6161ef.js";
-t = document.getElementsByTagName('script')[0]; t.parentNode.insertBefore(s, t);
-s.onload = function () {
-    window.dwidget = new window.DigitClock();
-    window.dwidget.init("dayspedia_widget_fc7390ee0a10720");
-};
